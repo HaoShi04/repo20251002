7470f1d9fc1f5c8064f8fa9ac3acac16e24b6153
HaoShi04 <shi95@sheridancollege.ca>
2025-10-02 08:22:15 -0400

    version 1 local

---
+++ src/main/java/git/practicegit2/PracticeGit2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package git.practicegit2;
+
+/**
+ *
+ * @author samuraishizuka
+ */
+public class PracticeGit2 {
+
+    public static void main(String[] args) {
+        System.out.println("This is version one");
+    }
+}
